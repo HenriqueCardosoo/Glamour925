c2a7b338b9dd72f835cb9e6bd21c6d2e38b2859b
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-08-07 12:50:33 -0300

    img de category separado em um objeto

---
+++ src/data/categoryData.js
@@ -0,0 +1,36 @@
+import images from '../assets/category-img/images';
+
+export const categoryData = {
+  aneis: [
+    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ],
+
+  pulseiras: [
+    { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ],
+  brincos: [
+    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ],
+  colares: [
+    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ],
+  pingentes: [
+    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ],
+  aliancas: [
+    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+  ]
+};
