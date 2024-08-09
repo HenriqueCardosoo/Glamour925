31abd8c66d8a4484120476d2ff2cbdb3d09f873b
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-08-08 21:49:13 -0300

    resolvido problema do scrool com useEffet

--- src/pages/CategoryPage/index.jsx
+++ src/pages/CategoryPage/index.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, CategoryItem, Image, Info, Title, Price, Installments, AddToCartButton, BackButton } from './styles';
 import { categoryData } from '../../data/categoryData';
@@ -10,25 +10,31 @@ const CategoryPage = ({ onAddToCart }) => {
   const { categoryName } = useParams();
   const items = categoryData[categoryName];
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [categoryName]); //usei useEffect para deixar o scrool sempre ao topo quando mudar de categoria
+
   if (!items) {
     return <div>Categoria não encontrada</div>;
   }
 
   return (
-    <Container>
-      {items.map((item, index) => (
-        <CategoryItem key={index}>
-          <Image src={item.imgSrc} alt={item.name} />
-          <Info>
-            <Title>{item.name}</Title>
-            <Price>{item.price}</Price>
-            <Installments>{item.installments}</Installments>
-            <AddToCartButton onClick={() => handleAddToCartWithAnimation(item, onAddToCart)}>Adicionar à Sacola</AddToCartButton>
-          </Info>
-        </CategoryItem>
-      ))}
+    <>
+      <Container>
+        {items.map((item, index) => (
+          <CategoryItem key={index}>
+            <Image src={item.imgSrc} alt={item.name} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Price>{item.price}</Price>
+              <Installments>{item.installments}</Installments>
+              <AddToCartButton onClick={() => handleAddToCartWithAnimation(item, onAddToCart)}>Adicionar à Sacola</AddToCartButton>
+            </Info>
+          </CategoryItem>
+        ))}
+      </Container>{' '}
       <BackButton onClick={() => window.history.back()}>Voltar</BackButton>
-    </Container>
+    </>
   );
 };
 
