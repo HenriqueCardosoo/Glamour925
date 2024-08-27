a71435ae0ee61911e1dde87d96d292f4624b9279
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-08-26 23:55:18 -0300

    corrigindo os nomes das joias

--- src/data/categoryData.js
+++ src/data/categoryData.js
@@ -3,34 +3,34 @@ import images from '../assets/category-img/images';
 export const categoryData = {
   aneis: [
     { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image1, name: 'Anel 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image1, name: 'Anel 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image1, name: 'Anel 3', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image1, name: 'Anel 4', price: 'R$6.650,00', installments: '10x de 665' }
   ],
 
   pulseiras: [
     { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image2, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image2, name: 'Pulseira 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image2, name: 'Pulseira 3', price: 'R$6.650,00', installments: '10x de 665' }
   ],
   brincos: [
-    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image3, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image3, name: 'brincos 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image3, name: 'brincos 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image3, name: 'brincos 3', price: 'R$6.650,00', installments: '10x de 665' }
   ],
   colares: [
-    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image4, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image4, name: 'colares 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image4, name: 'colares 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image4, name: 'colares 3', price: 'R$6.650,00', installments: '10x de 665' }
   ],
   pingentes: [
-    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image5, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image5, name: 'pingentes 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image5, name: 'pingentes 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image5, name: 'pingentes 3', price: 'R$6.650,00', installments: '10x de 665' }
   ],
   aliancas: [
-    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' },
-    { imgSrc: images.image6, name: 'Pulseira 1', price: 'R$6.650,00', installments: '10x de 665' }
+    { imgSrc: images.image6, name: 'aliancas 1', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image6, name: 'aliancas 2', price: 'R$6.650,00', installments: '10x de 665' },
+    { imgSrc: images.image6, name: 'aliancas 3', price: 'R$6.650,00', installments: '10x de 665' }
   ]
 };
