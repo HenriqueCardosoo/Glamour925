a1f5f871fd532af28255f9ff5161d20814e2b258
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-08-29 01:53:03 -0300

    Refatorei o tratamento de prop no componente AppContent

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Category from './components/Category';
 import ItemDetails from './components/ItemDetails';
@@ -15,6 +15,23 @@ import RandomItemsSection from './components/RamdomItemsSection';
 import PropTypes from 'prop-types';
 import Footer from './components/Footer';
 import PaymentPage from './pages/PaymentPage';
+
+const AppContent = ({ cartItems, isCartOpen, toggleCart, addItemToCart, removeItemFromCart, updateItemQuantity }) => {
+  const location = useLocation(); // Obter a localização atual da rota
+
+  return (
+    <>
+      {location.pathname !== '/payment' && <Navbar toggleCart={toggleCart} />}
+      <Routes>
+        <Route path="/" element={<Home onAddToCart={addItemToCart} />} />
+        <Route path="/item/:id" element={<ItemDetails onAddToCart={addItemToCart} />} />
+        <Route path="/category/:categoryName" element={<CategoryPage onAddToCart={addItemToCart} />} />
+        <Route path="/payment" element={<PaymentPage items={cartItems} onRemoveItem={removeItemFromCart} onUpdateQuantity={updateItemQuantity} />} />
+      </Routes>
+      <Cart items={cartItems} isOpen={isCartOpen} onClose={toggleCart} onRemoveItem={removeItemFromCart} onUpdateQuantity={updateItemQuantity} />
+    </>
+  );
+};
 
 export const MainApp = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -52,14 +69,7 @@ export const MainApp = () => {
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Router>
-        <Navbar toggleCart={toggleCart} />
-        <Routes>
-          <Route path="/" element={<Home onAddToCart={addItemToCart} />} />
-          <Route path="/item/:id" element={<ItemDetails onAddToCart={addItemToCart} />} />
-          <Route path="/category/:categoryName" element={<CategoryPage onAddToCart={addItemToCart} />} />
-          <Route path="/payment" element={<PaymentPage items={cartItems} onRemoveItem={removeItemFromCart} onUpdateQuantity={updateItemQuantity} />} />
-        </Routes>
-        <Cart items={cartItems} isOpen={isCartOpen} onClose={toggleCart} onRemoveItem={removeItemFromCart} OnUpdateQuantity={updateItemQuantity} />
+        <AppContent cartItems={cartItems} isCartOpen={isCartOpen} toggleCart={toggleCart} addItemToCart={addItemToCart} removeItemFromCart={removeItemFromCart} updateItemQuantity={updateItemQuantity} />
       </Router>
     </ThemeProvider>
   );
@@ -76,6 +86,23 @@ const Home = ({ onAddToCart }) => (
 
 Home.propTypes = {
   onAddToCart: PropTypes.func.isRequired
+};
+
+Cart.propTypes = {
+  items: PropTypes.array.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onRemoveItem: PropTypes.func.isRequired,
+  onUpdateQuantity: PropTypes.func.isRequired
+};
+
+AppContent.propTypes = {
+  cartItems: PropTypes.array.isRequired,
+  isCartOpen: PropTypes.bool.isRequired,
+  toggleCart: PropTypes.func.isRequired,
+  addItemToCart: PropTypes.func.isRequired,
+  removeItemFromCart: PropTypes.func.isRequired,
+  updateItemQuantity: PropTypes.func.isRequired
 };
 
 createRoot(document.getElementById('root')).render(
