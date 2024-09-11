2f42d32e261c74ec877c2662513425ee76ad0199
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-09-11 12:32:42 -0300

    Salvando os itens do cart no localStorage

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useCart = () => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    // Recupera os itens do localStorage na inicialização
+    const savedCartItems = localStorage.getItem('cartItems');
+    return savedCartItems ? JSON.parse(savedCartItems) : [];
+  });
   const [isCartOpen, setCartOpen] = useState(false);
 
   const toggleCart = () => {
@@ -12,29 +16,47 @@ export const useCart = () => {
     setCartItems((prevItems) => {
       const existingItemIndex = prevItems.findIndex((item) => item.name === newItem.name);
 
+      let updatedItems;
       if (existingItemIndex >= 0) {
-        const updatedItems = [...prevItems];
+        updatedItems = [...prevItems];
         updatedItems[existingItemIndex].quantity += 1;
-        return updatedItems;
       } else {
-        return [...prevItems, { ...newItem, quantity: 1 }];
+        updatedItems = [...prevItems, { ...newItem, quantity: 1 }];
       }
+
+      // Salva os itens atualizados no localStorage
+      localStorage.setItem('cartItems', JSON.stringify(updatedItems));
+      return updatedItems;
     });
 
     setCartOpen(true);
   };
 
   const removeItemFromCart = (indexToRemove) => {
-    setCartItems((prevItems) => prevItems.filter((_, index) => index !== indexToRemove));
+    setCartItems((prevItems) => {
+      const updatedItems = prevItems.filter((_, index) => index !== indexToRemove);
+
+      // Salva os itens atualizados no localStorage
+      localStorage.setItem('cartItems', JSON.stringify(updatedItems));
+      return updatedItems;
+    });
   };
 
   const updateItemQuantity = (index, quantity) => {
     setCartItems((prevItems) => {
       const updatedItems = [...prevItems];
       updatedItems[index].quantity = quantity;
+
+      // Salva os itens atualizados no localStorage
+      localStorage.setItem('cartItems', JSON.stringify(updatedItems));
       return updatedItems;
     });
   };
+
+  useEffect(() => {
+    // Sincroniza os itens do carrinho com o localStorage quando o estado do carrinho muda
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return {
     cartItems,
