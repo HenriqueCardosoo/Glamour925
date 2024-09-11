71bb287f3c89e1d3d1d085513c43301b9db2f585
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-09-11 13:58:49 -0300

    colocando total do pedido

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { CartContainer, CartHeader, CartFooter, ContinueButton, Overlay, CloseButton, GoToCartButton, CartScroll } from './styles';
 import { useNavigate } from 'react-router-dom';
 import CartItem from '../../components/CartItem'; // Importa o componente reutilizável
+import { calculateTotal } from '../../utils/calculateTotal';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 const Cart = ({ items, isOpen, onClose, onRemoveItem, onUpdateQuantity }) => {
   const navigate = useNavigate();
@@ -27,6 +29,8 @@ const Cart = ({ items, isOpen, onClose, onRemoveItem, onUpdateQuantity }) => {
     navigate('/payment');
   };
 
+  const total = calculateTotal(items);
+
   return (
     <>
       {/* resolvido o erro isOpen, usando Transient Props que começa com o prefixo "$" que ajuda a filtrar props não reconhecidos automaticamente*/}
@@ -37,6 +41,11 @@ const Cart = ({ items, isOpen, onClose, onRemoveItem, onUpdateQuantity }) => {
           <CloseButton onClick={onClose}>X</CloseButton>
         </CartHeader>
         <CartScroll>{items.length > 0 ? items.map((item, index) => <CartItem key={index} item={item} onRemove={() => onRemoveItem(index)} onUpdateQuantity={(quantity) => onUpdateQuantity(index, quantity)} />) : <p>Sua sacola está vazia.</p>}</CartScroll>
+        {items.length > 0 && (
+          <div>
+            <h3>Total: {formatCurrency(total)}</h3>
+          </div>
+        )}
         <CartFooter>
           <ContinueButton onClick={onClose}>Continuar Comprando</ContinueButton>
           <GoToCartButton onClick={handleGoToCart}>Ir para a Sacola</GoToCartButton>
